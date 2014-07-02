5bbb7724e1a49719dff475db21dbc648f45dcb4e
Wang Yingfei <clause@sjtu.edu.cn>
2014-07-02 20:10:45 +0800

    add 'more news' functionality

--- activity/views.py
+++ activity/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render_to_response
 from activity.models import Activity
 from activity.forms import ActivityForm
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core import serializers
 import datetime
 
 def write_news(request):
@@ -19,8 +20,14 @@ def write_news(request):
     return render_to_response('pushact.html', {'form': form})
 
 def news(request):
-    activities = Activity.objects.filter(group='news')[:6]
-    return render_to_response('actlist.html', {'activities', activities})
+    if request.method =='POST':
+        current_num = request.POST['i']
+        new = Activity.objects.filter(group='news')[current_num:current_num+4]
+        json = serializers.serialize('json', new)
+        return HttpResponse(json)
+    else:
+        activities = Activity.objects.filter(group='news')[:6]
+        return render_to_response('actlist.html', {'activities', activities})
 
 def activity_page(request, ID):
     activity = Activity.objects.get(itemID=ID)
