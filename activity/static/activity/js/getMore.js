c883493c8739456d7bdf97665208f7e98b3aba14
Zhao Haobo <zhbdebox@126.com>
2014-07-02 20:12:50 +0800

    edit getMore.js

--- activity/static/activity/js/getMore.js
+++ activity/static/activity/js/getMore.js
@@ -3,34 +3,37 @@
 var i=6;	//设定全局变量 i 初始值为 6
 
 function getMore() {	//定义函数
-	i++;	// i=i+1
-	$("#list").append(	//在id为list的元素中插入 需要jQuery 
-	' <div class="floatb"><div class="imgbox">图片</div><div class="titbox">标题</div><div class="contbox">内容</div><div class="morebox">...更多>></div></div>'	//第i个框，html语言用''括起来
-	);
+
+    $.ajax({
+        url: '/test1/',
+        type: "POST",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+
+        success: function (msg) {
+
+            var len = msg.length;
+
+            var j=0;
+            for (var j = 0; j < len; j++) {
+                $("#list").append(' <div class="floatb"><div class="imgbox">图片</div><div class="titbox">标题</div><div class="contbox">内容</div><div class="morebox">...更多>></div></div>');
+            }
+            for (var j = 0; j < len; j++) {
+                $("#list").append(' <div class="floatb"><div class="imgbox">图片</div><div class="titbox">标题</div><div class="contbox">内容</div><div class="morebox">...更多>></div></div>');
+            }
+
+            var fff = msg[j].pk;
+            $('#list').append(fff);
+        },
+
+        error: function (xmlHttpRequest, textStatus, errorThrown) {
+            alert(xmlHttpRequest.status);
+            alert(xmlHttpRequest.Readystate);
+            alert(textStatus);
+        }
+    });
+
+    i = i + 2;
 	
-	i++;	// i=i+1
-	$("#list").append(	//在id为list的元素中插入 需要jQuery 
-	' <div class="floatb"><div class="imgbox">图片</div><div class="titbox">标题</div><div class="contbox">内容</div><div class="morebox">...更多>></div></div>'	//第i个框，html语言用''括起来
-	);
-	
-	i++;	// i=i+1
-	$("#list").append(	//在id为list的元素中插入 需要jQuery 
-	' <div class="floatb"><div class="imgbox">图片</div><div class="titbox">标题</div><div class="contbox">内容</div><div class="morebox">...更多>></div></div>'	//第i个框，html语言用''括起来
-	);
-	
-	i++;	// i=i+1
-	$("#list").append(	//在id为list的元素中插入 需要jQuery 
-	' <div class="floatb"><div class="imgbox">图片</div><div class="titbox">标题</div><div class="contbox">内容</div><div class="morebox">...更多>></div></div>'	//第i个框，html语言用''括起来
-	);
-	
-	i++;	// i=i+1
-	$("#list").append(	//在id为list的元素中插入 需要jQuery 
-	' <div class="floatb"><div class="imgbox">图片</div><div class="titbox">标题</div><div class="contbox">内容</div><div class="morebox">...更多>></div></div>'	//第i个框，html语言用''括起来
-	);
-	
-	i++;	// i=i+1
-	$("#list").append(	//在id为list的元素中插入 需要jQuery 
-	' <div class="floatb"><div class="imgbox">图片</div><div class="titbox">标题</div><div class="contbox">内容</div><div class="morebox">...更多>></div></div>'	//第i个框，html语言用''括起来
-	);
 	
 };
