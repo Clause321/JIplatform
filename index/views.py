18d2c722c1eec1d6829987ec96810dd7b93ad166
Wang Yingfei <clause@sjtu.edu.cn>
2014-07-30 20:37:08 +0800

    order index activities in reverse order

--- index/views.py
+++ index/views.py
@@ -4,10 +4,9 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
 def index(request):
-    news = Activity.objects.filter(type='news').order_by('write_date')[:6]
-    activity = Activity.objects.filter(type='activity').order_by('write_date')[:6]
-    announce = Activity.objects.filter(type='announce').order_by('write_date')[:6]
-    #need reverse write_date
+    news = Activity.objects.filter(type='news').order_by('-write_date')[:6]
+    activity = Activity.objects.filter(type='activity').order_by('-write_date')[:6]
+    announce = Activity.objects.filter(type='announce').order_by('-write_date')[:6]
     return render_to_response('index.html', {'news': news,
                                              'activity': activity,
                                              'announce': announce,
