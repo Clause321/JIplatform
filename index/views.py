7534159850ea669a4fca28ca9d54389d502d3be0
Wang Yingfei <clause@sjtu.edu.cn>
2014-07-30 19:14:10 +0800

    delete index loop for news

--- index/views.py
+++ index/views.py
@@ -4,9 +4,9 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
 def index(request):
-    news = Activity.objects.filter(group='news').order_by('write_date')[:6]
-    activity = Activity.objects.filter(group='activity').order_by('write_date')[:6]
-    announce = Activity.objects.filter(group='announce').order_by('write_date')[:6]
+    news = Activity.objects.filter(type='news').order_by('write_date')[:6]
+    activity = Activity.objects.filter(type='activity').order_by('write_date')[:6]
+    announce = Activity.objects.filter(type='announce').order_by('write_date')[:6]
     return render_to_response('index.html', {'news': news,
                                              'activity': activity,
                                              'announce': announce,
