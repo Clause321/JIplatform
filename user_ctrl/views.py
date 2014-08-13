78872c3fc70e64d5e9ef1d4975f8fbee28d336da
Wang Yingfei <clause@sjtu.edu.cn>
2014-08-13 13:17:42 +0800

    modified user register, login and logout

--- user_ctrl/views.py
+++ user_ctrl/views.py
@@ -4,23 +4,26 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login, logout
 
 def register(request):
+    error = {'username': '', 'password1': '', 'password2': ''}
     if request.method == 'POST':
         new_user = UserCreationForm(request.POST)
-        error = new_user.get_validation_errors()
-        if not error:
+        # username, password1, password2
+        if new_user.isValid():
             new_user.save()
             return HttpResponseRedirect('/login/')
+        error = new_user.error
 
     return render_to_response('register.html', {'error': error})
 
 def login_view(request):
-    error = 0
+    error = {'username': '', 'password': ''}
     if request.method == 'POST':
         user = AuthenticationForm(request.POST)
-        error = user.get_validation_errors()
-        if not error:
+        # username, password
+        if user.isValid():
             login(request, user)
             return HttpResponseRedirect('/index/')
+        error = user.error
 
     return render_to_response('longin.html', {'error': error})
 
