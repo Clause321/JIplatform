b37dc5d2b5015fb448dfb64c377d03f5c2a5bbe2
Wang Yingfei <clause@sjtu.edu.cn>
2014-09-05 07:35:53 +0800

    add pfuser with manytomany relation with groups

--- user_ctrl/models.py
+++ user_ctrl/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from group.models import Group
 
-# Create your models here.
+class pfuser(models.Model):
+    User = models.OneToOneField(User, primary_key=True)
+    groups = models.ManyToManyField(Group)
+    groups_apply = models.ManyToManyField(Group)
+    groups_staff = models.ManyToManyField(Group)
+
+    def __unicode__(self):
+        return self.User.username
