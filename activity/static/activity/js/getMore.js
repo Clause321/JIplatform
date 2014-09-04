85f3e05c57e06b86a7768b899a48a60eaf2af838
Zhao Haobo <zhbdebox@126.com>
2014-09-04 18:53:32 -0400

    merge rBool to getMore

--- activity/static/activity/js/getMore.js
+++ activity/static/activity/js/getMore.js
@@ -4,12 +4,32 @@ var aaa=6;	//设定全局变量 i 初始值为 6
 
 function getMore() {	//定义函数
 
-    $.ajax({
+	thisURL = document.URL; 
+	var tOrp = "";
+	var subTyp = "";
+
+	for ( var i = 22; thisURL[i] != '/'; i++ )
+	{
+		tOrp = tOrp + thisURL[i];
+	} 
+
+	i++;
+	
+	if(tOrp == "group")
+{
+	while(thisURL[i] != '/')
+	{
+		subTyp = subTyp + thisURL[i];
+		i++;
+	}
+	
+$.ajax({
         url: '/news/',
         type: "POST",
         dataType: "json",
         data: [
                     { name: "aaa", value: aaa },
+					{ name: "group", value: tOrp},
         ],
 
         success: function (msg) {
@@ -33,6 +53,41 @@ function getMore() {	//定义函数
             alert(textStatus);
         }
     });
+	
+}
+else if(tOrp == "news" || tOrp == "activity" || tOrp == "announce" )
+{
+	$.ajax({
+        url: '/news/',
+        type: "POST",
+        dataType: "json",
+        data: [
+                    { name: "aaa", value: aaa },
+					{ name: "type", value: subTyp },
+        ],
+
+        success: function (msg) {
+
+            var len = msg.length;
+
+            for (var j = 0; j < len; j++) {
+                var title = msg[j].fields.title;
+                var content = msg[j].fields.content;
+                var img = msg[j].fields.pic;
+                $("#list").append(function(n) { return '<div class="floatb"><div class="imgbox"><img src="/media/' +img+ '"></img></div><div class="titbox">'+title+'</div><div class="contbox">'+content+'</div><div class="morebox">...更多>></div></div>' });
+            }
+
+            aaa = aaa + len;
+
+        },
+
+        error: function (xmlHttpRequest, textStatus, errorThrown) {
+            alert(xmlHttpRequest.status);
+            alert(xmlHttpRequest.Readystate);
+            alert(textStatus);
+        }
+    });
+}
 
 
     //var csrftoken = $.cookie('csrftoken');
